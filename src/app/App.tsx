f2359f33cf58c2f4dd5d4bd9653a5fedd909edd1
lorenzo.garcia.tlc@gmail.com <lorenzo.garcia.tlc@gmail.com>
2026-06-17 14:25:39 +0800

    refactor(app): move to src/app and delete unused app.css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
 import TestAllComponents from '@/components/TestComponents/TestAllComponents'
 
 //import { cn } from './lib/utils'
 //import {Button} from '@/components/ui/button'
 
-function App() {
+export default function App(): React.JSX.Element
+{
 
   return (
     <>
@@ -12,5 +12,3 @@ function App() {
     </>
   )
 }
-
-export default App
